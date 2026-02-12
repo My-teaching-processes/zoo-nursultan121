f215a3af35fd01201775adace45f12aacf9c26a5
Nurs <nursomar07@gmail.com>
2026-02-12 17:46:00 +0500

    1

--- src/zoo.py
+++ src/zoo.py
@@ -21,4 +21,45 @@ class Zoo:
         self._location = location
         self._animals: List[Animal] = []
 
-    # Здесь можно добавить нужные методы для управления животными в зоопарке (нужно чтобы тесты проходили)
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def location(self) -> str:
+        return self._location
+
+    @property
+    def animals(self) -> List[Animal]:
+        return list(self._animals)
+
+    def get_animal_count(self) -> int:
+        return len(self._animals)
+
+    def add_animal(self, animal: Animal) -> None:
+        self._animals.append(animal)
+
+    def remove_animal(self, animal: Animal) -> bool:
+        if animal in self._animals:
+            self._animals.remove(animal)
+            return True
+        return False
+
+    def find_animal_by_name(self, name: str) -> Optional[Animal]:
+        for animal in self._animals:
+            if animal.name == name:
+                return animal
+        return None
+
+    def get_species_count(self, species: str) -> int:
+        count = 0
+        for animal in self._animals:
+            if animal.species == species:
+                count += 1
+        return count
+
+    def get_all_sounds(self) -> List[str]:
+        sounds = []
+        for animal in self._animals:
+            sounds.append(animal.make_sound())
+        return sounds
